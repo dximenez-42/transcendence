e6a102ea6f140a58edf2124e52f180b986f66dd6
Dani X <danixidev@gmail.com>
2024-08-27 00:10:37 +0200

    Create games microservice

--- backend/games/games_app/urls.py
+++ backend/games/games_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('games', views.games, name='games'),  # Example endpoint
+    path('games/create', views.create, name='create'),
+    path('games/list', views.list, name='list'),
+    path('games/join', views.join, name='join'),
+    path('games/start', views.start, name='start'),
+    path('games/leave', views.leave, name='leave'),
 ]
--- postgres/init.sql
+++ postgres/init.sql
@@ -2,5 +2,39 @@ CREATE table IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE
+    email VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE table IF NOT EXISTS tournaments (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TYPE game_status AS ENUM ('open', 'ready', 'playing', 'finished');
+
+CREATE table IF NOT EXISTS games (
+    id SERIAL PRIMARY KEY,
+    host_id INT NOT NULL,
+    FOREIGN KEY (host_id) REFERENCES users(id),
+    status game_status DEFAULT 'open',
+    room_id VARCHAR(255) NOT NULL,
+    tournament_id INT,
+    FOREIGN KEY (tournament_id) REFERENCES tournaments(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE table IF NOT EXISTS game_players (
+    id SERIAL PRIMARY KEY,
+    game_id INT NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES games(id),
+    player_id INT NOT NULL,
+    FOREIGN KEY (player_id) REFERENCES users(id),
+    score INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
