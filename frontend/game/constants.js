5b764c18206283623f9a4e149f8057d7003b5554
Carlos Garcia Sanchez-Cano <carlosga@c1r2s1.42madrid.com>
2024-10-02 19:47:58 +0200

    game window amplified

--- frontend/game/constants.js
+++ frontend/game/constants.js
@@ -1,12 +1,12 @@
 export const padMoveStepLength = 3;
 export const tableHeight = 100;
-export const tableLength = 300;
+export const tableLength = 200;
 export const padLength = 30;
 export const padWidth = 10;
 export const FPS = 60;
-export const RGB_TABLE = 0x92DFFE;
-export const RGB_PAD_PLAYER = 0xB2E66D;
-export const RGB_PAD_ENAMY = 0x6BFDF2;
+export const RGB_TABLE = 0x90FF90;
+export const RGB_PAD_PLAYER = 0xFFFFFF;
+export const RGB_PAD_ENAMY = 0xFFFFFF;
 export const RGB_BALL = 0xD8DFE1;
 export const BALL_RADIUS = 5;
 export var   ballSpeed = 1;
