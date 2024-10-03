b4de1eeb0ee668d2e5494f14d2b1abfddadfac32
Pablo-Palma <pablopalmappr@gmail.com>
2024-10-03 16:36:49 +0200

    liveChat Folder removed

--- backend/chat/mywebsite/asgi.py
+++ backend/chat/mywebsite/asgi.py
@@ -5,8 +5,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
+# Configuramos el entorno y especificamos el módulo de configuración
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
 
+# redirigimos ciertas peticiones a un endpoint o a otro, las de websocket deben pasar por un Middleware de autenticacion
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket' : AuthMiddlewareStack(
