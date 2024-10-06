e6ad9fca2d2fe8e138c908459a0508fd8c1cbccb
Dani X <danixidev@gmail.com>
2024-10-06 17:25:08 +0200

    Edit user name and username

--- backend/users/users_app/urls.py
+++ backend/users/users_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('info/<int:id>', views.info, name='info'),
     path('me', views.me, name='me'),
+    path('edit', views.edit, name='edit'),
     path('list', views.list, name='list'),
     path('blocked', views.blocked, name='blocked'),
     path('block/<int:id>', views.block, name='block'),
