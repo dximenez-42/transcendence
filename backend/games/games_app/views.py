a536e8b5e0512c6b47fe3bace5a7839e7dc95eff
Dani X <danixidev@gmail.com>
2024-10-07 10:32:10 +0200

    Get and update game state

--- backend/games/games_app/views.py
+++ backend/games/games_app/views.py
@@ -48,7 +48,67 @@ def create(request):
         'room_id': game.room_id,
     }, status=201)
 
+@api_view(['GET'])
+def game(request, id):
+    game = Game.objects.filter(id=id).first()
+    if not game:
+        return JsonResponse({'error': 'Game not found.'}, status=404)
 
+    if game.status == 'open' or game.status == 'ready':
+        return JsonResponse({'error': 'Game not started.'}, status=400)
+    if game.status == 'finished':
+        return JsonResponse({'error': 'Game already finished.'}, status=400)
+
+    players = GamePlayer.objects.filter(game_id=game.id)
+    players_data = []
+    for player in players:
+        players_data.append({
+            'id': player.player_id,
+            'score': player.score,
+            'is_host': player.player_id == game.host_id,
+        })
+
+    return JsonResponse({
+        'game_id': game.id,
+        'room_id': game.room_id,
+        'status': game.status,
+        'players': players_data,
+        'is_host': game.host_id == request.user.id,
+    }, status=200)
+
+@api_view(['PUT'])
+def update(request, id):
+    data = json.loads(request.body)
+
+    if not data:
+        return JsonResponse({'error': 'Missing data'}, status=400)
+    
+    if not data['host_score'] or not data['guest_score'] or not data['finished'].exists():
+        return JsonResponse({'error': 'Missing fields in body'}, status=400)
+
+    game = Game.objects.filter(id=id).first()
+    if not game:
+        return JsonResponse({'error': 'Game not found.'}, status=404)
+    
+    # TODO: Review this
+    if game.host_id != request.user.id:
+        return JsonResponse({'error': 'Only the host can update the game.'}, status=400)
+    
+    if game.status != 'playing':
+        return JsonResponse({'error': 'Game is not being played.'}, status=400)
+    
+    players = GamePlayer.objects.filter(game_id=game.id)
+    for player in players:
+        if player.id == game.host_id:
+            player.score = data['host_score']
+            player.save()
+        else:
+            player.score = data['guest_score']
+            player.save()
+
+    if data['finished'] == True:
+        game.status = 'finished'
+        game.save()
 
 @api_view(['GET'])
 def list(request):
