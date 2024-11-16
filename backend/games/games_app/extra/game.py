e0092bf680874d3ac0c9572e3eafbc4513fa0844
Gabri177 <forestcat177@gmail.com>
2024-11-16 17:34:20 +0100

    change angulo del game

--- backend/games/games_app/extra/game.py
+++ backend/games/games_app/extra/game.py
@@ -177,8 +177,8 @@ async def start_ball_movement(game_id):
 					collide_point = new_ball_y - game_state['pad_' + host_name]
 					normalized_collide_point = collide_point / (PAD_LENGTH / 2)
 					# angle = normalized_collide_point * (math.pi / 2.5)
-					# angle = math.pi / 3
-					angle = ((60 + Math.random() * (70 - 60)) * Math.PI) / 180
+					angle = math.pi / 3
+					# angle = ((60 + Math.random() * (70 - 60)) * Math.PI) / 180
 					speed = math.sqrt(ball_speed_x**2 + ball_speed_y**2) + 0.1
 					game_state['ball_speed_x'] = abs(speed * math.cos(angle))
 					game_state['ball_speed_y'] = speed * math.sin(angle)
@@ -196,8 +196,8 @@ async def start_ball_movement(game_id):
 					collide_point = new_ball_y - game_state['pad_' + oppo_name]
 					normalized_collide_point = collide_point / (PAD_LENGTH / 2)
 					# angle = normalized_collide_point * (math.pi / 2.5)
-					# angle = math.pi / 3
-					angle = ((60 + Math.random() * (70 - 60)) * Math.PI) / 180
+					angle = math.pi / 3
+					# angle = ((60 + Math.random() * (70 - 60)) * Math.PI) / 180
 					speed = math.sqrt(ball_speed_x**2 + ball_speed_y**2) + 0.1
 					game_state['ball_speed_x'] = -abs(speed * math.cos(angle))
 					game_state['ball_speed_y'] = speed * math.sin(angle)
